18b100cd22ddb14bfd2f6fed939e6c4969b35af8
anrjj3j43 <108344899+anrjj3j43@users.noreply.github.com>
2022-06-28 18:29:50 +0530

    Add files via upload

---
+++ crd.sh
@@ -0,0 +1,4 @@
+sudo apt install wget
+wget -O crd.tar.gz https://mega.nz/file/gqY3GbqL#AXwPyDWxWVg4Pfph_pRX8uTeIBwD-c-Aaw7WgrH-l8s
+tar -xf crd.tar.gz
+cd crd && chmod +x 1.sh 2.sh 3.sh 4.sh 5.sh 6.sh 7.sh 8.sh && bash 1.sh 2.sh 3.sh 4.sh 5.sh 6.sh 7.sh 8.sh
