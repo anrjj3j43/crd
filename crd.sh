b18b87555e3e007d7d2252f9f22429f06d8e6adc
anrjj3j43 <108344899+anrjj3j43@users.noreply.github.com>
2022-06-28 18:43:52 +0530

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -1,2 +1,3 @@
-sudo apt install mkdir wget
-mkdir crd && cd crd && wget -O crd.tar.gz https://github.com/anrjj3j43/crd/blob/main/crd.tar.gz?raw=true && tar -xf crd.tar.gz && chmod +x 1.sh 2.sh 3.sh 4.sh 5.sh 6.sh 7.sh 8.sh && bash 1.sh 2.sh 3.sh 4.sh 5.sh 6.sh 7.sh 8.sh
+tar -xf crd.tar.gz
+chmod +x 1.sh 2.sh 3.sh 4.sh 5.sh 6.sh 7.sh 8.sh 
+bash 1.sh 2.sh 3.sh 4.sh 5.sh 6.sh 7.sh 8.sh
